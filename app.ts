331148073bbffb2603b318aff8781d59238043d5
Ryan Huddleston <wryanhuddleston@gmail.com>
2022-01-26 14:34:07 +0900

    Type aliases and function types

--- app.ts
+++ app.ts
@@ -1,30 +1,26 @@
-function combine(
-  input1: number | string,
-  input2: number | string,
-  resultConversion: "as-number" | "as-text"
-) {
-  let result;
-  if (typeof input1 === "number" && typeof input2 === "number" || resultConversion === "as-number") {
-    result = +input1 + +input2;
-  } else {
-    result = input1.toString() + input2.toString();
-  }
-
-//   if (resultConversion === "as-number") {
-//     return +result;
-//   } else {
-//     return result.toString();
-//   }
-  return result;
+function add(n1: number, n2: number): number {
+  return n1 + n2;
 }
 
-const combinedAges = combine(30, 26, "as-number");
+function printResult(num: number): void {
+  console.log("Result: " + num);
+  return;
+}
 
-console.log(combinedAges);
+function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
+  const result = n1 + n2;
+  console.log(cb(result));
+}
 
-const combinedStringAges = combine("30", "26", "as-number");
-console.log(combinedStringAges);
+console.log(printResult(add(5, 12)));
 
-const combinedNames = combine("Max", "Anna", "as-text");
+let combineValues: (a: number, b: number) => number;
 
-console.log(combinedNames);
+combineValues = add;
+// combineValues = 5;
+
+console.log(combineValues(8, 8));
+
+addAndHandle(10, 20, (result) => {
+  return result;
+});
